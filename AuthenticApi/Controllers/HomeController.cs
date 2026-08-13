2869adf979a8b38d99a0c3fe14fdbca66134c37f
Micael Souza <micael.ns@hotmail.com>
2026-08-13 01:49:16 -0300

    chore: criado uma pagina default simples

---
+++ AuthenticApi/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AuthenticApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Home");
+        }
+
+    }
+}
