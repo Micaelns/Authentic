cac075305d187949546e3ffd00827baddcef1a26
Micael Souza <micael.ns@hotmail.com>
2026-08-12 23:35:58 -0300

    Chore: instalação e configuração do Unity - Injeção DI

---
+++ AuthenticApi/App_Start/UnityConfig.cs
@@ -0,0 +1,26 @@
+using AuthenticApi.App_Data;
+using AuthenticApi.Services.UserService;
+using System.Web.Http;
+using System.Web.Mvc;
+using Unity;
+using Unity.WebApi;
+
+namespace AuthenticApi
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            // MVC 5
+            DependencyResolver.SetResolver(
+                new Unity.Mvc5.UnityDependencyResolver(container)
+            );
+
+            // Web API 2
+            GlobalConfiguration.Configuration.DependencyResolver = 
+                new UnityDependencyResolver(container);
+        }
+    }
+}
